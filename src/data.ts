ea634c1b01e9dbbaebc88e41238365b02e0fa8c1
monokemonoke <kawabataryuhei@gmail.com>
2022-11-25 12:41:33 +0900

    dataをもつファイルを分割

---
+++ src/data.ts
@@ -0,0 +1,41 @@
+export type Spot = {
+	name: string;
+	imgUrl: string;
+};
+
+export const data: Spot[] = [
+	{
+		name: '清水寺',
+		imgUrl:
+			'https://s3-ap-northeast-1.amazonaws.com/thegate/2021/05/19/10/51/59/Kiyomizudera_Temple_Title.jpg',
+	},
+	{
+		name: '産寧坂',
+		imgUrl:
+			'https://kyoto-meguru.com/wp-content/uploads/2021/01/DC_18273645.jpg',
+	},
+	{
+		name: '八坂神社',
+		imgUrl:
+			'https://rimage.gnst.jp/gurutabi.gnavi.co.jp/image/public/img/article/c7/42/art002448/article_art002448_og_facebook.jpg?1617699002',
+	},
+	{
+		name: '伏見稲荷大社',
+		imgUrl: 'https://souda-kyoto.jp/blog/ea410e000000c9rx-img/2.jpg',
+	},
+	{
+		name: '金閣寺',
+		imgUrl:
+			'https://s3-ap-northeast-1.amazonaws.com/thegate/2021/03/29/19/44/10/Kinkakuji_Temple_Sharidenn.jpg',
+	},
+	{
+		name: '円山公園',
+		imgUrl:
+			'https://www.city.kyoto.lg.jp/kensetu/cmsfiles/contents/0000257/257049/maruyama.JPG',
+	},
+	{
+		name: '銀閣寺',
+		imgUrl:
+			'https://www.shokoku-ji.jp/wp-content/themes/shokokuji/assets/img/og/ginkakuji.jpg',
+	},
+];
