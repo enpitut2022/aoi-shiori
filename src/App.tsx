a2d24d9ce25289cce69220fa6f30705457a55cd2
monokemonoke <kawabataryuhei@gmail.com>
2022-12-07 13:08:09 +0900

    使用していないコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,8 @@
-import { useState } from "react";
 import "./App.css";
-import { useDnDSort } from "./useDnDSort";
-import { Spot, data } from "./data";
-import { Container, Draggable } from "react-smooth-dnd";
 //@ts-ignore
 import Cards from "./cards";
 
-interface Result {
-  // key propsに設定する文字列
-  key: string;
-  // 配列内の画像URL文字列
-  value: Spot;
-  // ドラッグ＆ドロップ処理で使うイベント関数を返す関数
-  events: {
-    ref: (value: any) => void;
-    onMouseDown: (event: any) => void;
-  };
-}
-
 function App() {
-  const results = useDnDSort(data);
-
-  const hoge = (
-    <div>
-      <p>ドラッグ&ドロップで順番を入れ替えられます！</p>
-      <div className="column">
-        <div className="timeLine">
-          <div>朝</div>
-          <div>昼</div>
-          <div>夜</div>
-        </div>
-        <div>
-          <p>1日目</p>
-          {/* 配列の要素を表示する */}
-          {results.map((item) => (
-            <div className="card" key={item.key} {...item.events}>
-              <p>{item.value.name}</p>
-              <img src={item.value.imgUrl} alt="ソート可能な画像" />
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-
   return (
     <div>
       <Cards />
