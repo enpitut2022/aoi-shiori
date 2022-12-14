97cfdd181d62b6abacafd4688c88fb52749bdf06
monokemonoke <kawabataryuhei@gmail.com>
2022-12-14 13:46:19 +0900

    formatをかけた

--- src/spotCard.tsx
+++ src/spotCard.tsx
@@ -1,22 +1,22 @@
 import { Spot } from "./data";
 
 type Props = {
-    props: {
-        className: string;
-        style: Object;
-    };
-    data: Spot;
+  props: {
+    className: string;
+    style: Object;
+  };
+  data: Spot;
 };
 
 const SpotCard = (props: Props) => {
-    return (
-        <>
-            <div className={props.props.className} style={props.props.style}>
-                <p>{props.data.name}</p>
-                <img src={props.data.imgUrl} alt="" />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={props.props.className} style={props.props.style}>
+        <p>{props.data.name}</p>
+        <img src={props.data.imgUrl} alt="" />
+      </div>
+    </>
+  );
 };
 
 export default SpotCard;
