7d027757fefe43434292ca619303ef935782d3cf
monokemonoke <kawabataryuhei@gmail.com>
2022-12-16 15:59:28 +0900

    スワイプできた

--- src/spotCards.tsx
+++ src/spotCards.tsx
@@ -1,5 +1,34 @@
-export const SpotCards = () => {
-    return <div>hogehoge</div>
+import { Container, Draggable } from "react-smooth-dnd";
+import { data } from "./data";
+import { Spot } from "./data";
+
+type Props = Spot;
+
+const SpotCard = (props: Props) => {
+  return (
+    <>
+      <div>
+        <p>{props.name}</p>
+        <img src={props.imgUrl} alt="" />
+      </div>
+    </>
+  );
 };
+
+const SpotCards = () => {
+  return (
+    <>
+      <div>
+        <Container onDrop={console.log}>
+          {data.map(spot => (
+            <Draggable key={spot.id}>
+              <SpotCard {...spot} />
+            </Draggable>
+          ))}
+        </Container>
+      </div>
+    </>
+  )
+}
 
 export default SpotCards;
