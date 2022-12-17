a1d4c6456ec3b377dac13168ccfb7052b7997b8a
monokemonoke <kawabataryuhei@gmail.com>
2022-12-17 18:02:52 +0900

    カードを動かしたら出るバグを低減

--- src/spotCards.tsx
+++ src/spotCards.tsx
@@ -44,27 +44,57 @@ const SpotCards = () => {
     return result;
   };
 
+  // XXX: 応急処置用 後々取りたい
+  const notUndefined = <T,>(arr: T[]): T[] => {
+    return arr.filter(v => !!v);
+  }
+
   // ドロップされたときにspotの順番を更新する
   const onDropHandler = (columnName: string, e: DropResult) => {
     if (columnName === 'spots') {
       setDatas((old): Data => {
-        return { spots: applyDrag(old.spots, e), candidate: old.candidate };
-      });
-
-      setDistance(() => {
-        return updateDistance(datas.spots);
+        return { spots: applyDrag(notUndefined(old.spots), e), candidate: notUndefined(old.candidate) };
       });
     }
 
     if (columnName === 'candidate') {
       setDatas((old): Data => {
-        return { spots: old.spots, candidate: applyDrag(old.candidate, e) };
+        return { spots: notUndefined(old.spots), candidate: applyDrag(notUndefined(old.candidate), e) };
       });
     }
+
+    setDistance(() => {
+      return updateDistance(notUndefined(datas.spots));
+    });
   }
 
   const getCardPayload = (columnName: string, index: number): Spot => {
     return columnName === 'spots' ? datas.spots[index] : datas.candidate[index];
+  }
+
+  const genSpotAndDistance = (data: Spot | string) => {
+    if (typeof data === 'string') {
+      return (
+        <div key={Math.floor(Math.random() * 99999)}>
+          <DistanceBlock distance={data} />
+        </div>
+      )
+    }
+    return (
+      <Draggable key={Math.floor(Math.random() * 99999)}>
+        <SpotCard {...data} />
+      </Draggable>
+    )
+  }
+
+  const cncatSpotAndDistance = (spots: Spot[], distance: string[]): (Spot | string)[] => {
+    let res: (Spot | string)[] = [];
+    for (let i = 0; i < distance.length; i++) {
+      res.push(spots[i]);
+      res.push(distance[i]);
+    }
+    res.push(spots[spots.length - 1]);
+    return res
   }
 
   return (
@@ -80,14 +110,7 @@ const SpotCards = () => {
             }
             onDrop={e => onDropHandler('spots', e)}
           >
-            {datas.spots.map((spot, index) => (
-              <>
-                <Draggable key={Math.floor(Math.random() * 99999)}>
-                  <SpotCard {...spot} />
-                </Draggable>
-                {console.log(distance[index])}
-              </>
-            ))}
+            {cncatSpotAndDistance(datas.spots, distance).map((data) => genSpotAndDistance(data))}
           </Container>
         </div>
 
@@ -101,7 +124,7 @@ const SpotCards = () => {
             }
             onDrop={e => onDropHandler('candidate', e)}
           >
-            {datas.candidate.map(spot => (
+            {notUndefined(datas.candidate).map(spot => (
               <Draggable key={Math.floor(Math.random() * 99999)}>
                 <SpotCard {...spot} />
               </Draggable>
