b1bdb10df43f2ad01cef6421b7ddeba40f41b934
Yusuke Ise <shosuke447@gmail.com>
2022-12-28 12:45:20 +0900

    ロゴが見えるようになった

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import SpotCards from "./spotCards";
 // @ts-ignore
 import Cards from "./cards";
+import logo_noname from "./assets/logo_noname.png";
+import logo_onlyname from "./assets/logo_onlyname.png";
 
 function App() {
   return (
     <>
     <div className="logo">
-      <img src="/src/assets/logo_noname.png" />
-      <img src="/src/assets/logo_onlyname.png" />
+      <img src={logo_noname} />
+      <img src={logo_onlyname} />
     </div>
       <div>
         <SpotCards />
